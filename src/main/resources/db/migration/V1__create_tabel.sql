b699001cbf8c6004abe3b55010b82d86e2f6930d
Aibar <aibar03aibar@gmail.com>
2025-01-25 21:47:53 +0500

    update V1__create_tabel.sql and add V2__create_role.sql

--- src/main/resources/db/migration/V1__create_tabel.sql
+++ src/main/resources/db/migration/V1__create_tabel.sql
@@ -1,14 +1,15 @@
+-- Создание таблицы для сущности Role с автоинкрементом
 CREATE TABLE role (
-                      id BIGINT PRIMARY KEY,
-                      role VARCHAR(255) NOT NULL UNIQUE
+                      id BIGSERIAL PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL UNIQUE
 );
 
--- Создание таблицы для сущности User
-CREATE TABLE "user" (
-                        id BIGINT PRIMARY KEY,
-                        username VARCHAR(255) NOT NULL,
-                        email VARCHAR(255) NOT NULL UNIQUE,
-                        password VARCHAR(255) NOT NULL
+-- Создание таблицы для сущности User с автоинкрементом
+CREATE TABLE users (
+                       id BIGSERIAL PRIMARY KEY,
+                       username VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL
 );
 
 -- Создание связующей таблицы для ManyToMany отношения между User и Role
@@ -16,6 +17,6 @@ CREATE TABLE user_roles (
                             user_id BIGINT NOT NULL,
                             role_id BIGINT NOT NULL,
                             PRIMARY KEY (user_id, role_id),
-                            FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE,
+                            FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
                             FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE
 );
